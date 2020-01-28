1b5e97c418da408bd04ed2c4a0420ddd353b841b
hscom96 <hscom96@gmail.com>
2020-01-28 15:42:17 +0900

    initial commit

---
+++ src/test/java/com/neoga/autionnara/AutionnaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoga.autionnara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutionnaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
