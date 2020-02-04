98b23717d7ed60bd38552540e06a702e5e35dd45
ki1556ki <ki1556ki@gmail.com>
2020-02-05 07:31:16 +0900

    feat: create category api

---
+++ src/main/java/com/neoga/autionnara/category/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.neoga.autionnara.category.service;
+
+import com.neoga.autionnara.category.domain.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getCategoryList();
+}
--- src/main/java/com/neoga/autionnara/category/service/CategoryServiceImpl.java
+++ src/main/java/com/neoga/autionnara/category/service/CategoryServiceImpl.java
@@ -1,9 +1,20 @@
 package com.neoga.autionnara.category.service;
 
-import lombok.RequiredArgsConstructor;
+import com.neoga.autionnara.category.domain.Category;
+import com.neoga.autionnara.category.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
-public class CategoryServiceImpl {
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getCategoryList() {
+        return categoryRepository.findAll();
+    }
 }
