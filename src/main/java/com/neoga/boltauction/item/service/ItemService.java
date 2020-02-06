c5cd283a7fe93d475a2fc40bba2e8b301a2c10fe
ki1556ki <ki1556ki@gmail.com>
2020-02-06 20:25:33 +0900

    feat: edit item api code

--- src/main/java/com/neoga/boltauction/item/service/ItemService.java
+++ src/main/java/com/neoga/boltauction/item/service/ItemService.java
@@ -1,22 +1,30 @@
-package com.neoga.autionnara.item.service;
+package com.neoga.boltauction.item.service;
 
-import com.neoga.autionnara.item.domain.Item;
+import com.neoga.boltauction.item.domain.Item;
+import com.neoga.boltauction.item.domain.ItemDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemService {
     List<Item> getItemList();
 
     void insertItem(Item item);
 
-    Optional<Item> getItem(Long id);
+    ResponseEntity getItem(Long id);
 
-    void updateItem(Item item);
-
-    void deleteItem(Item item);
+    ResponseEntity<Object> deleteItem(Long id);
 
     Page<Item> findAll(Pageable pageable);
+
+    ResponseEntity getItems(Long categoryId, Pageable pageable, PagedResourcesAssembler<ItemDto> assembler);
+
+    ResponseEntity insertItem(ItemDto itemDto, Errors errors/*, MultipartFile[] images*/);
+
+    ResponseEntity updateItem(Long id, ItemDto itemDto);
 }
--- src/main/java/com/neoga/boltauction/item/service/ItemServiceImpl.java
+++ src/main/java/com/neoga/boltauction/item/service/ItemServiceImpl.java
@@ -1,23 +1,41 @@
-package com.neoga.autionnara.item.service;
+package com.neoga.boltauction.item.service;
 
-import com.neoga.autionnara.item.domain.Item;
-import com.neoga.autionnara.item.repository.ItemRepository;
+import com.neoga.boltauction.category.domain.Category;
+import com.neoga.boltauction.category.repository.CategoryRepository;
+import com.neoga.boltauction.item.controller.ItemController;
+import com.neoga.boltauction.item.domain.Item;
+import com.neoga.boltauction.item.domain.ItemDto;
+import com.neoga.boltauction.item.domain.ItemEntityModel;
+import com.neoga.boltauction.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.TimeZone;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
     private final ItemRepository itemRepository;
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public List<Item> getItemList() {
@@ -32,22 +50,105 @@ public class ItemServiceImpl implements ItemService {
     }
 
     @Override
-    public Optional<Item> getItem(Long id) {
-        return itemRepository.findById(id);
+    public ResponseEntity getItem(Long id) {
+        Optional<Item> optionalItem = itemRepository.findById(id);
+        if (!optionalItem.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Item findItem = optionalItem.get();
+        ItemDto itemDto = modelMapper.map(findItem, ItemDto.class);
+        ItemEntityModel itemEntityModel = new ItemEntityModel(itemDto);
+        itemEntityModel.add(new Link("/").withRel("profile"));
+
+        return ResponseEntity.ok(itemEntityModel);
     }
 
     @Override
-    public void updateItem(Item item) {
-        itemRepository.save(item);
+    public ResponseEntity updateItem(Long id, ItemDto itemDto) {
+        Optional<Item> optionalItem = itemRepository.findById(id);
+        if (!optionalItem.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Item findItem = optionalItem.get();
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.map(itemDto, findItem);
+        itemRepository.save(findItem);
+        modelMapper.map(findItem, itemDto);
+        ItemEntityModel itemEntityModel = new ItemEntityModel(itemDto);
+        itemEntityModel.add(new Link("/").withRel("profile"));
+
+        return ResponseEntity.ok(itemEntityModel);
     }
 
     @Override
-    public void deleteItem(Item item) {
-        itemRepository.deleteById(item.getId());
+    public ResponseEntity<Object> deleteItem(Long id) {
+        Optional<Item> optionalItem = itemRepository.findById(id);
+        if (!optionalItem.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        itemRepository.delete(optionalItem.get());
+
+        return null;
     }
 
     @Override
     public Page<Item> findAll(Pageable pageable) {
         return itemRepository.findAll(pageable);
+    }
+
+    @Override
+    public ResponseEntity getItems(Long categoryId, Pageable pageable, PagedResourcesAssembler<ItemDto> assembler) {
+        Page<Item> findItems = itemRepository.findAllByOrderByCreateDateAsc(pageable);
+
+        Page<ItemDto> itemDtoPage = findItems.map(item -> {
+                ItemDto itemDto = modelMapper.map(item, ItemDto.class);
+                itemDto.setItemName(item.getName());
+                itemDto.setCategoryId(item.getCategory().getId());
+                return itemDto;
+            }
+        );
+
+        PagedModel<EntityModel<ItemDto>> entityModels = assembler.toModel(itemDtoPage, i -> new EntityModel(i));
+        entityModels.forEach(entityModel -> entityModel.add(linkTo(methodOn(ItemController.class).getItem(entityModel.getContent().getItemId())).withRel("item-detail")));
+        entityModels.add(new Link("/").withRel("profile"));
+
+        return ResponseEntity.ok(entityModels);
+    }
+
+    @Override
+    public ResponseEntity insertItem(ItemDto itemDto, Errors errors/*, MultipartFile[] images*/) {
+        Item item = new Item();
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        item = modelMapper.map(itemDto, Item.class);
+        item.setName(itemDto.getItemName());
+        item.setCreateDate(LocalDateTime.now());
+        Optional<Category> optionalCategory = categoryRepository.findById(itemDto.getCategoryId());
+        if (!optionalCategory.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+
+        item.setCategory(optionalCategory.get());
+
+        Item saveItem = itemRepository.save(item);
+
+        ItemDto mappedItemDto = modelMapper.map(saveItem, ItemDto.class);
+        mappedItemDto.setItemId(saveItem.getId());
+        mappedItemDto.setItemName(saveItem.getName());
+        mappedItemDto.setCategoryId(saveItem.getCategory().getId());
+
+        WebMvcLinkBuilder selfLinkBuilder =linkTo(ItemController.class).slash(itemDto.getItemId());
+        URI createdUri = selfLinkBuilder.toUri();
+        ItemEntityModel itemEntityModel = new ItemEntityModel(mappedItemDto);
+        itemEntityModel.add(linkTo(ItemController.class).withRel("query-events"));
+        itemEntityModel.add(selfLinkBuilder.withRel("update-event"));
+        itemEntityModel.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
+
+        return ResponseEntity.created(createdUri).body(itemEntityModel);
     }
 }
