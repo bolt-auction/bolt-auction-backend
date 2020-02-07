825c2b64cf8691355d86d2cf15abbe87154591ff
hscom96 <hscom96@gmail.com>
2020-02-08 04:43:53 +0900

    apply swagger to proj

--- src/main/java/com/neoga/boltacution/security/controller/AuthController.java
+++ src/main/java/com/neoga/boltacution/security/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.neoga.boltacution.memberstore.member.domain.Members;
 import com.neoga.boltacution.memberstore.member.domain.Role;
 import com.neoga.boltacution.memberstore.member.repository.MemberRepository;
 import com.neoga.boltacution.security.service.JwtTokenService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,16 +16,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 
 @RequiredArgsConstructor
+@Api(tags = {"Auth"})
 @RestController
 @RequestMapping(value = "/api")
-public class SignController {
+public class AuthController {
 
     private final MemberRepository memberRepo;
     private final JwtTokenService jwtTokenService;
     private final PasswordEncoder passwordEncoder;
 
+    @ApiOperation(value = "로그인", notes = "로그인을 하며 jwt 토큰 발행")
     @GetMapping(value = "/login")
-    public ResponseEntity login(@RequestParam String email, @RequestParam String passwd) {
+    public ResponseEntity login(@ApiParam(value = "회원아이디", required = true)@RequestParam String email,
+                                @ApiParam(value = "비밀번호", required = true) @RequestParam String passwd) {
         Members member = memberRepo.findByEmail(email).orElseThrow(CEmailLoginFailedException::new);
         if (!passwordEncoder.matches(passwd, member.getPasswd()))
             throw new CEmailLoginFailedException();
@@ -30,8 +36,11 @@ public class SignController {
         return ResponseEntity.ok(token);
     }
 
+    @ApiOperation(value = "회원가입", notes = "정보를 입력받아 회원가입")
     @PostMapping(value = "/signup")
-    public ResponseEntity signin(@RequestParam String email, @RequestParam String passwd,  @RequestParam String name) {
+    public ResponseEntity signin(@ApiParam(value = "회원아이디", required = true)@RequestParam String email,
+                                 @ApiParam(value = "비밀번호", required = true)@RequestParam String passwd,
+                                 @ApiParam(value = "닉네임", required = true)@RequestParam String name) {
         memberRepo.save(Members.builder()
                 .email(email)
                 .passwd(passwordEncoder.encode(passwd))
