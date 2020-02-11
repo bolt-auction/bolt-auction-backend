284fa6e6b749bafdfeb95cdb7d93197574c9ca56
ki1556ki <ki1556ki@gmail.com>
2020-02-12 01:04:24 +0900

    fix: remove unused import

--- src/main/java/com/neoga/boltauction/bid/service/BidService.java
+++ src/main/java/com/neoga/boltauction/bid/service/BidService.java
@@ -1,9 +1,6 @@
 package com.neoga.boltauction.bid.service;
 
-import com.neoga.boltauction.bid.domain.Bid;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
 
 public interface BidService {
     public ResponseEntity getBidList(Long id);
