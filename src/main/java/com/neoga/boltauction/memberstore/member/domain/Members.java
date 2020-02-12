b9d83344ef0444d569d13f6507d5f51e8172c6bf
hscom96 <hscom96@gmail.com>
2020-02-12 12:48:56 +0900

    fix: Merge error

--- src/main/java/com/neoga/boltauction/memberstore/member/domain/Members.java
+++ src/main/java/com/neoga/boltauction/memberstore/member/domain/Members.java
@@ -2,14 +2,18 @@ package com.neoga.boltauction.memberstore.member.domain;
 
 import com.neoga.boltauction.memberstore.store.domain.Store;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter @Setter
 @Entity
+@Builder
 public class Members {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
