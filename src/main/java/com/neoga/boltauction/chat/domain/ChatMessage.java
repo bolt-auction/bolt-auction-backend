2d4fc40cec02b84019b3bae1a87abb6409cdd5b8
hscom96 <hscom96@gmail.com>
2020-02-26 23:34:25 +0900

    feat: Create chat domain

---
+++ src/main/java/com/neoga/boltauction/chat/domain/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.neoga.boltauction.chat.domain;
+
+import com.neoga.boltauction.memberstore.member.domain.Members;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createDt;
+    private boolean isRead;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_id")
+    private ChatRoom chatRoom;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Members sender;
+}
