85e171d5dc4b66da5a3e8e9327e8bb7805746fb4
hscom96 <hscom96@gmail.com>
2020-03-01 20:29:24 +0900

    feat : add pageable feat / api info to chatRoom

--- src/main/java/com/neoga/boltauction/chat/service/ChatRoomService.java
+++ src/main/java/com/neoga/boltauction/chat/service/ChatRoomService.java
@@ -1,12 +1,12 @@
 package com.neoga.boltauction.chat.service;
 
 import com.neoga.boltauction.chat.domain.ChatRoom;
-import com.neoga.boltauction.chat.dto.CreateRoomDto;
+import com.neoga.boltauction.chat.dto.CreateRoomRequestDto;
 import com.neoga.boltauction.chat.repository.ChatRoomRepository;
 import com.neoga.boltauction.item.domain.Item;
-import com.neoga.boltauction.memberstore.member.domain.Members;
 import com.neoga.boltauction.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -21,7 +21,7 @@ public class ChatRoomService {
     private final EntityManager em;
 
     // 1:1 채팅방 생성
-    public ChatRoom createChatRoom(CreateRoomDto createRoomDto){
+    public ChatRoom createChatRoom(CreateRoomRequestDto createRoomDto){
         Item item = em.getReference(Item.class, createRoomDto.getItemId());
         ChatRoom newRoom = chatRoomRepository
                 .save(ChatRoom.builder()
@@ -36,9 +36,9 @@ public class ChatRoomService {
     }
 
     //자신의 채팅방참여 리스트 반환
-    public List<ChatRoom> findChatRoomList(){
+    public List<ChatRoom> findChatRoomList(Pageable pageable){
         Long memberId = authService.getLoginInfo().getMemberId();
-        List<ChatRoom> chatList = chatRoomRepository.findChatRoomList(memberId);
+        List<ChatRoom> chatList = chatRoomRepository.findChatRoomList(memberId, pageable);
         return chatList;
     }
 }
