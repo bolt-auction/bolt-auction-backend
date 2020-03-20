30f31b2cdd2e576c0eaa063711cf814f71cc1707
hscom96 <hscom96@gmail.com>
2020-03-20 17:08:38 +0900

    feat: Add gateway setting

--- gateway/src/main/java/com/neoga/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/neoga/gateway/GatewayApplication.java
@@ -1,7 +1,5 @@
 package com.neoga.gateway;
 
-import com.neoga.gateway.config.RibbonConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,16 +8,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableEurekaClient
-//@RibbonClients(defaultConfiguration = RibbonConfig.class)
 @SpringBootApplication
 public class GatewayApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "/home/ec2-user/app/config/boltauction/server-secret.yml,"
             + "classpath:common-application.yml,"
             + "classpath:bootstrap.yml,"
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
+            + "classpath:application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayApplication.class)
