7a74a18b65660b2cfa00d030c6104bdc0f36bb4d
ki1556ki <ki1556ki@gmail.com>
2020-03-23 16:54:46 +0900

    feat: item api show super category

---
+++ platform/src/main/java/com/neoga/platform/category/util/CategorySerializer.java
@@ -0,0 +1,20 @@
+package com.neoga.platform.category.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.neoga.platform.category.domain.Category;
+
+import java.io.IOException;
+
+public class CategorySerializer extends JsonSerializer<Category> {
+    @Override
+    public void serialize(Category category, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("id", category.getId());
+        jsonGenerator.writeStringField("name", category.getName());
+        jsonGenerator.writeNumberField("supCategoryId", category.getSupCategory().getId());
+        jsonGenerator.writeStringField("supCategoryName", category.getSupCategory().getName());
+        jsonGenerator.writeEndObject();
+    }
+}
